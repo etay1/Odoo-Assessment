f9a80206b346231db5837025aaac6e0127b25b57
Elijah Tay <98202550+etay1@users.noreply.github.com>
2022-12-01 00:00:26 -0500

    Add files via upload

--- Flexbox-Grid_Practice/Odoo Assessment/random.js
+++ Flexbox-Grid_Practice/Odoo Assessment/random.js
@@ -3,6 +3,29 @@ let randomString = randomNumber.toString();
 let randomInt = parseInt(randomString);
 
 function height() {
+    let boxString
+    let randomNumber
+    let randomString
+
+    //create three sets of numbers each randomized 
+    //and floored at 32 (height of h1)
+    //concat px for style.height to receieve a 
+    //string with pixels as a parameter
+    
+    for (let i = 1; i <= 3; i++) {
+        randomNumber = Math.floor(Math.random() * 150 + 32)
+        randomString = randomNumber.toString().concat("px")
+        document.getElementById("box".concat(i)).style.height = randomString
+    }
+
+    /*
+    verifying pixels are being randomized
+    console.log(randomString1)
+    console.log(randomString2)
+    console.log(randomString3)
+    */
+
+    /*
     let randomNumber1 = Math.floor(Math.random() * 150 + 32);
     let randomNumber2 = Math.floor(Math.random() * 150 + 32);
     let randomNumber3 = Math.floor(Math.random() * 150 + 32);
@@ -11,16 +34,13 @@ function height() {
     let randomString2 = randomNumber2.toString();
     let randomString3 = randomNumber3.toString();
 
-    //concat px for style.height to receieve a 
-    //string with pixels as a parameter
     randomString1 =  randomString1.concat("px")
     randomString2 =  randomString2.concat("px")
     randomString3 =  randomString3.concat("px")
+    
+    document.getElementById("box1").style.height = randomString 
+    document.getElementById("box2").style.height = randomString
+    document.getElementById("box3").style.height = randomString
+    */
 
-
-    //verifying pixels are being randomized
-    console.log(randomString)
-    document.getElementById("box1").style.height = randomString1 
-    document.getElementById("box2").style.height = randomString2
-    document.getElementById("box3").style.height = randomString3
 }
