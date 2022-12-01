c09bdfc703f0b7a81589bcb5aaa0a9034ae6d75f
Elijah Tay <98202550+etay1@users.noreply.github.com>
2022-12-01 00:56:26 -0500

    Add files via upload

--- Flexbox-Grid_Practice/Odoo Assessment/random.js
+++ Flexbox-Grid_Practice/Odoo Assessment/random.js
@@ -11,14 +11,18 @@ function height() {
     //and floored at 32 (height of h1)
     //concat px for style.height to receieve a 
     //string with pixels as a parameter
-    
+
     for (let i = 1; i <= 3; i++) {
         randomNumber = Math.floor(Math.random() * 150 + 32)
         randomString = randomNumber.toString().concat("px")
         document.getElementById("box".concat(i)).style.height = randomString
     }
+}
 
-    /*
+    
+/* ----------------------------------------------------------
+
+
     verifying pixels are being randomized
     console.log(randomString1)
     console.log(randomString2)
@@ -41,6 +45,67 @@ function height() {
     document.getElementById("box1").style.height = randomString 
     document.getElementById("box2").style.height = randomString
     document.getElementById("box3").style.height = randomString
-    */
+    
 
 }
+
+function createDropBuffer(beforeElement) {
+    var buffer = document.createElement("div");
+    buffer.setAttribute("draggable", "true");
+    buffer.setAttribute("class", "buffer");
+    buffer.setAttribute('id', 'moving-buffer')
+    beforeElement.before(buffer);
+    buffer.addEventListener('dragenter', (bufferEvent) => {
+        bufferEvent.preventDefault();
+    });
+    buffer.addEventListener('dragover', (bufferEvent) => {
+        bufferEvent.preventDefault();
+    });
+    buffer.addEventListener('dragleave', (bufferEvent) => {
+        bufferEvent.preventDefault();
+        $('#moving-buffer').remove();
+    });
+    buffer.addEventListener('drop', (bufferEvent) => {
+        bufferEvent.preventDefault();
+        let moved = document.getElementById(bufferEvent.dataTransfer.getData('text'));
+        bufferEvent.target.parentNode.insertBefore(moved, bufferEvent.target);
+        buffer.remove();
+    });
+}
+
+function setUpDrag(element) {
+    element.addEventListener('dragstart', (event) => {
+        event.dataTransfer.setData('text/plain', event.target.id);
+        var buffer = document.createElement("div");
+        buffer.setAttribute("draggable", "true");
+        buffer.setAttribute("class", "bottom-buffer");
+        $('.column').each((index, element) => {
+            var clonedBuffer = buffer.cloneNode(true);
+            clonedBuffer.addEventListener('dragenter', (bufferEvent) => {
+                bufferEvent.preventDefault();
+                $('#moving-buffer').remove();
+                createDropBuffer(bufferEvent.target);
+            });
+            $(element).append(clonedBuffer);
+        });
+    });
+    element.addEventListener('drag', (event) => {
+        $(element).hide();
+    });
+    element.addEventListener('dragenter', (event) => {
+        event.preventDefault();
+        $('#moving-buffer').remove();
+        createDropBuffer(event.target);
+    });
+    element.addEventListener('dragend', (event) => {
+        $('.buffer, .bottom-buffer').remove();
+        $(element).show();
+    });
+}
+
+$('.cell').each((index, element) => {
+    let height = Math.floor(Math.random() * (100) + 100)
+    let cell = $(element);
+    cell.css('height',  `${height}px`);
+    setUpDrag(element)
+});*/
