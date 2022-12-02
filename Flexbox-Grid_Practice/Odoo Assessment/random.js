128c5d467c15eb3a8335af98e5ade29755c342f7
Elijah Tay <98202550+etay1@users.noreply.github.com>
2022-12-02 03:27:28 -0500

    Add files via upload

--- Flexbox-Grid_Practice/Odoo Assessment/random.js
+++ Flexbox-Grid_Practice/Odoo Assessment/random.js
@@ -12,7 +12,7 @@ function height() {
     //concat px for style.height to receieve a 
     //string with pixels as a parameter
 
-    for (let i = 1; i <= 3; i++) {
+    for (let i = 1; i <= 9; i++) {
         randomNumber = Math.floor(Math.random() * 150 + 32)
         randomString = randomNumber.toString().concat("px")
         document.getElementById("box".concat(i)).style.height = randomString
