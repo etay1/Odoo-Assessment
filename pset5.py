1f054a20dba02fa0a396dd585faf2b09c55fd700
Elijah Tay <98202550+etay1@users.noreply.github.com>
2022-12-05 03:59:35 -0500

    Update pset5.py

--- pset5.py
+++ pset5.py
@@ -1,6 +1,6 @@
 x = input("test string: ")
 
 if x.startswith("\"") and x.endswith("\"") or x.startswith("\'") and x.endswith("\'"):
-    print("match")
+    print("matches")
 else:
     print("does not match")
