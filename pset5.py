4175005ca7a017d7f563dcdee8ec242c52ef21c2
Elijah Tay <98202550+etay1@users.noreply.github.com>
2022-12-05 20:48:59 -0500

    Update pset5.py

--- pset5.py
+++ pset5.py
@@ -1,6 +1,21 @@
-x = input("test string: ")
+import re
 
-if x.startswith("\"") and x.endswith("\"") or x.startswith("\'") and x.endswith("\'"):
-    print("matches")
-else:
-    print("does not match")
+#test data
+s = "'Hi'"
+s1 = "'Hi\""
+# regex expression notations => "^STARTS.*ENDS$"
+if(re.search("^'.*'$" or "^\".*\"$", s)):
+    print("Match")
+else: print("Does not match")
+
+if(re.search("^'.*'$" or "^\".*\"$", s1)):
+    print("Match")
+else: print("Does not match")
+
+#input testing
+# userString = input("Check if a string is encapsulated in either\n"
+#                    "Two double quotations OR\n"
+#                    "Two single quotations:\n")
+# if(re.search("^'.*'$" or "^\".*\"$", userString)):
+#     print("Match")
+# else: print("Does not match")
